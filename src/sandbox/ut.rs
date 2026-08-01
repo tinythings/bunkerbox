59c788f04e149b293124992fb7271cf94b6ae46a
Bo Maryniuk <bo@maryniuk.net>
2026-08-01 18:18:40 +0200

    Add unit tests

--- src/sandbox/ut.rs
+++ src/sandbox/ut.rs
@@ -1,6 +1,6 @@
 use super::*;
 use std::collections::BTreeMap;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[test]
 fn test_parse_profile() {
@@ -8,10 +8,9 @@ fn test_parse_profile() {
 name: test
 bin:
   ls: /usr/bin/ls
-ro:
-  - /lib
-rw:
-  - "${HOME}/.cache"
+paths:
+  - src: /lib
+  - src: .cache
 env:
   FOO: bar
 network: none
@@ -20,8 +19,7 @@ shell: /bin/sh
     let profile = parse_profile_yaml(yaml).unwrap();
     assert_eq!(profile.name, "test");
     assert_eq!(profile.bin.get("ls").unwrap(), &std::path::PathBuf::from("/usr/bin/ls"));
-    assert_eq!(profile.ro.len(), 1);
-    assert_eq!(profile.rw.len(), 1);
+    assert_eq!(profile.paths.len(), 2);
     assert!(matches!(profile.network, NetworkMode::None));
 }
 
@@ -34,8 +32,7 @@ fn test_merge_profiles() {
             m.insert("cmd1".into(), "/usr/bin/cmd1".into());
             m
         },
-        ro: vec!["/lib".into()],
-        rw: vec!["/cache".into()],
+        paths: vec![ProfilePath { src: "/lib".into(), dst: None }, ProfilePath { src: "cache".into(), dst: None }],
         env: {
             let mut m = BTreeMap::new();
             m.insert("A".into(), "1".into());
@@ -51,8 +48,7 @@ fn test_merge_profiles() {
             m.insert("cmd2".into(), "/usr/bin/cmd2".into());
             m
         },
-        ro: vec!["/usr/lib".into()],
-        rw: vec!["/other".into()],
+        paths: vec![ProfilePath { src: "/usr/lib".into(), dst: None }, ProfilePath { src: "other".into(), dst: None }],
         env: {
             let mut m = BTreeMap::new();
             m.insert("B".into(), "2".into());
@@ -61,10 +57,11 @@ fn test_merge_profiles() {
         network: NetworkMode::None,
         shell: "/bin/dash".into(),
     };
-    let merged = MergedProfile::from_profiles(&[p1, p2]).unwrap();
+    let merged = MergedProfile::from_profiles(&[p1, p2], Some(Path::new("/home/test"))).unwrap();
     assert_eq!(merged.bin.len(), 2);
-    assert_eq!(merged.ro.len(), 2);
-    assert_eq!(merged.rw.len(), 2);
+    assert_eq!(merged.paths.len(), 4);
+    assert!(!merged.paths[0].writable);
+    assert_eq!(merged.paths[1].destination, PathBuf::from("/home/cache"));
     assert_eq!(merged.env.len(), 2);
     assert_eq!(merged.shell, PathBuf::from("/bin/dash"));
     assert_eq!(merged.name, "a+b");
@@ -109,8 +106,7 @@ fn reject_nul_in_profile_environment_key_without_returning_nul() {
     let profile = Profile {
         name: "test".into(),
         bin: Default::default(),
-        ro: Vec::new(),
-        rw: Vec::new(),
+        paths: Vec::new(),
         env: [("BAD\0KEY".into(), "value".into())].into_iter().collect(),
         network: NetworkMode::None,
         shell: "/bin/sh".into(),
@@ -120,4 +116,41 @@ fn reject_nul_in_profile_environment_key_without_returning_nul() {
     assert!(err.contains("environment key"));
     assert!(err.contains("NUL"));
     assert!(!err.contains('\0'));
+}
+
+#[test]
+fn resolve_paths_with_standard_and_explicit_destinations() {
+    let profile = Profile {
+        name: "paths".into(),
+        bin: Default::default(),
+        paths: vec![
+            ProfilePath { src: ".cargo".into(), dst: None },
+            ProfilePath { src: "/home/bo/.rustup".into(), dst: None },
+            ProfilePath { src: "/opt/sdk/include".into(), dst: None },
+            ProfilePath { src: "/opt/sdk/lib".into(), dst: Some("/toolchain/lib".into()) },
+        ],
+        env: Default::default(),
+        network: NetworkMode::None,
+        shell: "/bin/sh".into(),
+    };
+
+    let merged = MergedProfile::from_profiles(&[profile], Some(Path::new("/home/bo"))).unwrap();
+
+    assert_eq!(merged.paths[0].source, PathBuf::from("/home/bo/.cargo"));
+    assert_eq!(merged.paths[0].destination, PathBuf::from("/home/.cargo"));
+    assert!(merged.paths[0].writable);
+    assert_eq!(merged.paths[1].destination, PathBuf::from("/home/.rustup"));
+    assert!(merged.paths[1].writable);
+    assert_eq!(merged.paths[2].destination, PathBuf::from("/opt/sdk/include"));
+    assert!(!merged.paths[2].writable);
+    assert_eq!(merged.paths[3].destination, PathBuf::from("/toolchain/lib"));
+    assert!(!merged.paths[3].writable);
+}
+
+#[test]
+fn all_builtin_profiles_use_paths() {
+    for name in ["rust", "make", "node", "go", "python"] {
+        let profile = resolve_profile(name, Path::new("/nonexistent")).unwrap();
+        assert!(!profile.paths.is_empty(), "{name} has no paths");
+    }
 }
